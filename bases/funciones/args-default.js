e034d6958d3cbabffd5b18c3ce6b95e90239282e
Felipe-Mina <88908796+Felipe-Mina@users.noreply.github.com>
2022-09-27 17:48:19 -0300

    Aprendiendo argumentos por default

---
+++ bases/funciones/args-default.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    const fullName = (firstName, lastName, upper = false) => {
+        if (upper) {
+            return `${firstName} ${lastName || 'No lastname'}`.toUpperCase();
+        }
+        else {
+            return `${firstName} ${lastName || 'No lastname'}`;
+        }
+        ;
+    };
+    const name = fullName('Tony', 'Stark');
+    console.log({ name });
+})();
