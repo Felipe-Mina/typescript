f1907a276a059ad09dc61f2f23a31c13eae52d5a
Felipe-Mina <88908796+Felipe-Mina@users.noreply.github.com>
2022-10-11 21:47:13 -0300

    Practico de tipo de objetos

---
+++ bases/trabajos/trabajo de objetos/app.js
@@ -0,0 +1,49 @@
+"use strict";
+var _a;
+// Objetos
+const batimovil = {
+    carroceria: "Negra",
+    modelo: "6x6",
+    antibalas: true,
+    pasajeros: 4
+};
+const bumblebee = {
+    carroceria: "Amarillo con negro",
+    modelo: "4x2",
+    antibalas: true,
+    pasajeros: 4,
+    disparar() {
+        console.log("Disparando");
+    }
+};
+console.log(bumblebee);
+console.log(batimovil);
+console.log((_a = bumblebee.disparar) === null || _a === void 0 ? void 0 : _a.call(bumblebee));
+const villanos = [{
+        nombre: "Lex Luthor",
+        edad: 54,
+        mutante: false
+    }, {
+        nombre: "Erik Magnus Lehnsherr",
+        edad: 49,
+        mutante: true
+    }, {
+        nombre: "James Logan",
+        edad: undefined,
+        mutante: true
+    }];
+console.log(villanos);
+const charles = {
+    poder: "psiquico",
+    estatura: 1.78
+};
+const apocalipsis = {
+    lider: true,
+    miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
+};
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique;
+mystique = charles;
+console.log(mystique);
+mystique = apocalipsis;
+console.log(mystique);
